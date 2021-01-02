ea76a0cb8fb742caaa3389297e8f3306617ce3e7
Jeff St. Jean <jeffrey@stjean.ca>
2021-01-02 14:51:48 -0500

    cross platform endian include

--- tools/cFS-GroundSystem/Subsystems/cmdUtil/cmdUtil.c
+++ tools/cFS-GroundSystem/Subsystems/cmdUtil/cmdUtil.c
@@ -33,13 +33,32 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <endian.h>
+// #include <endian.h>
 #include <getopt.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <errno.h>
 #include "SendUdp.h"
+#ifndef FINK_ENDIANDEV_PKG_ENDIAN_H
+#define FINK_ENDIANDEV_PKG_ENDIAN_H 1
+    #ifndef APPLE
+        #warning "This header file (endian.h) is MacOS X specific.\n"
+    #endif /* APPLE */
+    #include <libkern/OSByteOrder.h>
+    #define htobe16(x) OSSwapHostToBigInt16(x)
+    #define htole16(x) OSSwapHostToLittleInt16(x)
+    #define be16toh(x) OSSwapBigToHostInt16(x)
+    #define le16toh(x) OSSwapLittleToHostInt16(x)
+    #define htobe32(x) OSSwapHostToBigInt32(x)
+    #define htole32(x) OSSwapHostToLittleInt32(x)
+    #define be32toh(x) OSSwapBigToHostInt32(x)
+    #define le32toh(x) OSSwapLittleToHostInt32(x)
+    #define htobe64(x) OSSwapHostToBigInt64(x)
+    #define htole64(x) OSSwapHostToLittleInt64(x)
+    #define be64toh(x) OSSwapBigToHostInt64(x)
+    #define le64toh(x) OSSwapLittleToHostInt64(x)
+#endif /* FINK_ENDIANDEV_PKG_ENDIAN_H */
 
 /*
  * Defines
